a77465dff30301eb745be86fe86fcb5734d5e0c3
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2020-11-12 16:37:45 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,168 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumStruct2
+{
+    class Program
+    {
+        #region Opgave
+//        1. Maak een struct ‘Tijd’ die de tijd kan bijhouden(uren en minuten). Maak een methode
+//‘BerekenDuur’ die de duur kan berekenen tussen 2 tijden.Schrijf een testprogramma in Main
+//om de methode BerekenDuur te testen.(maak ook methodes om tijden in te kunnen lezen en
+//te kunnen tonen)
+//2. Breid het programma uit vorige oefening uit met een struct Afspraak, bestaande uit een
+//begintijd en een eindtijd.Maak een methode die een afspraak kan tonen: begintijd, eindtijd
+//en de duur van de afspraak.Maak ook een methode die een nieuwe afspraak kan inlezen.
+//Schrijf een testprogramma in Main met een menu met 3 opties.Afspraken toevoegen,
+//afspraken tonen en de laatste optie om het programma af te sluiten.
+//3. Breid het programma uit vorige oefening uit.Een afspraak heeft een AfspraakType(enum). Er
+//zijn 2 mogelijkheden: prive of werk.
+//Breid het menu uit met 2 mogelijkheden om enkel de prive afspraken te zien of enkel de
+//werk afspraken.
+        #endregion
+
+        static void Main(string[] args)
+        {
+            List<Afspraak> afspraken = new List<Afspraak>();
+            List<Afspraak> wanted = new List<Afspraak>();
+            afspraken.Add(new Afspraak("test1", AfspraakType.Prive, new Tijd(30, 15), new Tijd(15, 16)));
+            afspraken.Add(new Afspraak("test2", AfspraakType.Werk, new Tijd(30, 14), new Tijd(15, 16)));
+            afspraken.Add(new Afspraak("test3", AfspraakType.Prive, new Tijd(23, 09), new Tijd(30, 17)));
+            afspraken.Add(new Afspraak("test4", AfspraakType.Werk, new Tijd(00, 9), new Tijd(30, 17)));
+            afspraken.Add(new Afspraak("test5", AfspraakType.Prive, new Tijd(48, 6), new Tijd(15, 16)));
+            int menuOption;
+            bool menuOpen = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Wat wilt u doen?\n1: afspraak toevoegen\n2: alle afspraken tonen\n3: alle werk afspraken tonen\n4: alle privé afspraken tonen\n5: stoppen");
+                menuOption = int.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (menuOption)
+                {
+                    case 1:
+                        afspraken.Add(AskAfspraak());
+                        menuOpen = true;
+                        break;
+                    case 2:
+                        foreach (var item in afspraken)
+                        {
+                            Console.WriteLine(item.ToString() + Environment.NewLine);
+                        }
+                        menuOpen = true;
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        wanted = afspraken.Where(e => e.AfspraakType == AfspraakType.Werk).ToList();
+                        foreach (var item in wanted)
+                        {
+                            Console.WriteLine(item.ToString() + Environment.NewLine);
+                        }
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        wanted = afspraken.Where(e => e.AfspraakType == AfspraakType.Prive).ToList();
+                        foreach (var item in wanted)
+                        {
+                            Console.WriteLine(item.ToString() + Environment.NewLine);
+                        }
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        Console.WriteLine("Doei");
+                        menuOpen = false;
+                        break;
+                    default:
+                        menuOpen = true;
+                        break;
+                }
+            } while (menuOpen);
+            Console.ReadLine();
+        }
+
+        private static Afspraak AskAfspraak()
+        {
+            Console.WriteLine("Geef de naam van de afspraak");
+            string afspraakNaam = Console.ReadLine();
+            Tijd starttijd = AskTijd();
+            Tijd eindtijd = AskTijd();
+            Console.WriteLine("wilt u \n1: werkafspraak\n2: priveafspraak");
+            int choice = int.Parse(Console.ReadLine());
+            AfspraakType afspraakType = (AfspraakType)choice;
+            Console.Clear();
+            return new Afspraak(afspraakNaam, afspraakType, starttijd, eindtijd);
+        }
+
+        //SOC
+        //SEPERATIONS OF CONCERNS
+        private static Tijd AskTijd()
+        {
+            int[] time;
+            Console.WriteLine("Geef de tijd (HH:MM)");
+            time = Array.ConvertAll(Console.ReadLine().Split(':'), e => int.Parse(e));
+            return new Tijd(time[1], time[0]);
+        }
+    }
+    struct Tijd
+    {
+        #region PROPERTIES
+        public int Hours;
+        public int Minutes;
+        #endregion
+        #region CONSTRUCTOR
+        public Tijd(int minutes, int hours)
+        {
+            Minutes = minutes;
+            Hours = hours;
+        }
+        #endregion
+        #region METHODS
+        public override string ToString()
+        {
+            return $"{Hours:D2}:{Minutes:D2}";
+        }
+        #endregion
+    }
+    struct Afspraak
+    {
+        public string Naam;
+        public AfspraakType AfspraakType;
+        public Tijd Starttijd;
+        public Tijd Eindtijd;
+        public int Duur;
+        public Afspraak(string naam, AfspraakType afspraakType, Tijd starttijd, Tijd eindtijd)
+        {
+            AfspraakType = afspraakType;
+            Naam = naam;
+            Starttijd = starttijd;
+            Eindtijd = eindtijd;
+            Duur = BerekenDuur(starttijd, eindtijd);
+        }
+        private static int BerekenDuur(Tijd starttijd, Tijd eindtijd)
+        {
+            int totalStartMinutes, totalEndMinutes, duur;
+            totalStartMinutes = TotalMinutes(starttijd);
+            totalEndMinutes = TotalMinutes(eindtijd);
+            duur = totalEndMinutes - totalStartMinutes;
+            return duur;
+        }
+        private static int TotalMinutes(Tijd tijd)
+        {
+            return tijd.Hours * 60 + tijd.Minutes;
+        }
+        public override string ToString()
+        {
+            return $"Afspraak: {Naam}\nAfspraakType: {AfspraakType}\nStarttijd: {Starttijd}\nEindtijd: {Eindtijd}\nDuur: {Duur / 60:D2}:{Duur % 60:D2}";
+        }
+    }
+    enum AfspraakType
+    {
+        Werk,
+        Prive
+    }
+}
---
+++ Enum_Struct/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumStructOEF1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //D
+            Temperatuur temp;
+            //I
+            Console.WriteLine("Deze applicatie kan temperaturen omrekenen tussen C F en K");
+            Console.WriteLine("Geef de om te rekenen temperatuur in, gevolgd door C, F of K");
+            string[] input = Console.ReadLine().Split(' ');
+            temp = new Temperatuur(
+                (Eenheid)Enum.Parse(typeof(Eenheid), input[1]),
+                double.Parse(input[0]));
+
+            //P&O
+            Console.WriteLine(temp.CalculateTemperatures(temp.Eenheid, temp.Value));
+            Console.ReadLine();
+        }
+    }
+    public struct Temperatuur
+    {
+        public Eenheid Eenheid;
+        public double Value;
+        public Temperatuur(Eenheid eenheid, double value)
+        {
+            Eenheid = eenheid;
+            Value = value;
+        }
+        public string CalculateTemperatures(Eenheid eenheid, double value)
+        {
+            double celcius, kelvin, farenheit;
+            switch (eenheid)
+            {
+                case Eenheid.C:
+                    celcius = value;
+                    farenheit = value * 1.8 + 32;
+                    kelvin = value + 273.15;
+                    break;
+                case Eenheid.F:
+                    celcius = (value - 32) / 1.8;
+                    farenheit = value;
+                    kelvin = (value - 32) * (5 / 9) + 273.15;
+                    break;
+                case Eenheid.K:
+                    celcius = value - 273.15;
+                    farenheit = (value - 273.15) * (9 / 5) + 32;
+                    kelvin = value;
+                    break;
+                default:
+                    celcius = 0;
+                    kelvin = 0;
+                    farenheit = 0;
+                    break;
+            }
+            return $"Omgerekend is dit:\n{celcius}°C\n{kelvin}K\n{farenheit}°F";
+        }
+    }
+    public enum Eenheid
+    {
+        C,
+        F,
+        K
+    }
+}
+
+
+
+//namespace Enum_Struct
+//{
+
+//    public enum EenheidEnum
+//    {
+//        C = 1,
+//        F,
+//        K,
+//    }
+
+//    class Program
+//    {
+
+//        //        Schrijf een programma dat de temperatuurconversie kan doen tussen °C, °F en K.
+//        //        Je geeft de temperatuur in in de temperatuurschaal naar jou keuze en het programma converteert
+//        //        de temperatuur naar de andere 2 schalen.
+//        //        Maak een enum eenheid waarin je de eenheid(C/F/K) kan bijhouden
+//        //        Maak een struct Temperatuur die een eenheid en een waarde bijhoud
+
+
+//        static void Main(string[] args)
+//        {
+//            Temperatuur = temp;
+//            Console.WriteLine("Deze applicatie kan temperaturen omrekenen in C/F/K\n" +
+//            "Geef de om te rekenen temperatuur in, gevolgd door C, F, K");
+//            string[] input = Console.ReadLine().Split(' ');
+//            temp = new Temperatuur(
+//            (EenheidEnum)Enum.Parse(typeof(EenheidEnum), input[1]),
+//            double.Parse(input[0]));
+
+
+
+//            public struct Temperatuur
+//        {
+//            public EenheidEnum Eenheid;
+//            public int Waarde;
+//            public Temperatuur(int waarde, EenheidEnum eenheid);
+//            {
+//                Waarde = waarde;
+//                Eenheid = eenheid;
+//            }
+
+//    }
+
+
+//            #region ongebruikt maar misschien nuttig
+//            //EenheidEnum c = (EenheidEnum)1;     //c = waarde van enum plaats 1
+//            //Console.WriteLine(c);
+
+//            //EenheidEnum f = EenheidEnum.F;      //f = waarde van enum met waarde F
+//            //Console.WriteLine(f);
+
+//            //EenheidEnum k = EenheidEnum.K;      //k = plaats van enum K = 3
+//            //Console.WriteLine((int)k);
+
+
+
+
+//            //Temperatuur s1 = new Temperatuur();
+//            //s1.Eenheid = 'K';
+//            //s1.Waarde = +273;
+
+//            //Temperatuur s2 = new Temperatuur();
+//            //s2.Eenheid = 'C';
+//            //s2.Waarde = -273;
+
+//            //Temperatuur s3 = new Temperatuur();
+//            //s3.Eenheid = 'F';
+//            //s3.Waarde = 11;
+
+//            //Console.WriteLine("Deze applicatie kan temperaturen omrekenen in C/F/K\n" +
+//            //"Geef de om te rekenen temperatuur in, gevolgd door C, F, K");
+
+//            //int temp = Convert.ToInt32(Console.ReadLine());
+//            //var input = Enum.Parse(typeof(EenheidEnum), Console.ReadLine());
+
+//            //Console.Clear();
+//            //Console.WriteLine($"{temp} {input}");
+//            #endregion
+
+//            foreach (int i in Enum.GetValues(typeof(EenheidEnum)))              //Enum.GetValues(typof     Toont alle plaatsen 0,1,2,3,4(int)     ENUMARATE VALUES
+//            {
+//                Console.WriteLine(i);
+//            }
+
+//foreach (string j in Enum.GetNames(typeof(EenheidEnum)))            //Enum.GetNames(typeof     Toont alle dagen(strings)                ENUMARATE NAMES
+//{
+//    Console.WriteLine(j);
+//}
+
+//switch (switch_on)
+//{
+//    case
+//            }
+//        }
+
+//        public struct Temperatuur
+//{
+
+//    public int Waarde;
+//    public string Eenheid;
+//    //C = (F-32)/1.8 
+//    //F = C* 1.8 + 32
+//    //C = K – 273.15 
+//    //K = C + 273.15
+
+//}
+//    }
+//}
